0a24395f8e611714483105e0437370cd61d84c3d
besil <s.bernardinello@reply.it>
2015-01-08 17:49:22 +0100

    Started Connected Components

---
+++ src/centrality/ConnectedComponents.java
@@ -0,0 +1,19 @@
+package centrality;
+
+import graph.IGraph;
+import graphcomputer.VertexFunction;
+import it.unimi.dsi.fastutil.ints.Int2IntMap;
+
+public class ConnectedComponents extends VertexFunction {
+
+	@Override
+	public void apply(IGraph graph, int n) {
+		
+	}
+
+	@Override
+	public Int2IntMap getResult() {
+		return null;
+	}
+
+}
